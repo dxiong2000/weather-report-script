1be25cc9821b2b684de02b0f8558a78f7fab9735
dxiong2000 <dxiong2000@gmail.com>
2018-12-20 16:54:50 -0800

    big bug test

--- WeatherUpdate.py
+++ WeatherUpdate.py
@@ -14,10 +14,10 @@ data = apiResponse.json()
 summary = 'The temperature is %s with a high of %s and a low of %s' % (data['currently']['temperature'], data['daily']['data'][0]['temperatureHigh'], data['daily']['data'][0]['temperatureLow'])
 
 
-
 client = boto3.client('sns','us-east-1')
 
 phoneNumber = #phonenumber
 message = summary
 
-client.publish(PhoneNumber=phoneNumber, Message=message)
+for i in range(0, 10):
+	client.publish(PhoneNumber=phoneNumber, Message=message)
