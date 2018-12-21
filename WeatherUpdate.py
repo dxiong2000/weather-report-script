c41b30099224d2df4779c9506ce183cd5ce31395
dxiong2000 <dxiong2000@gmail.com>
2018-12-20 16:17:20 -0800

    testing git

--- WeatherUpdate.py
+++ WeatherUpdate.py
@@ -17,7 +17,7 @@ summary = 'The temperature is %s with a high of %s and a low of %s' % (data['cur
 
 client = boto3.client('sns','us-east-1')
 
-phoneNumber = #HIDDEN
+phoneNumber = #phone number
 message = summary
 
 client.publish(PhoneNumber=phoneNumber, Message=message)
